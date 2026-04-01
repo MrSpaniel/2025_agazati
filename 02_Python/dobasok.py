a05a80b1b1eb4254c0e0a527c8658f931b4d06ae
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2026-04-01 11:15:02 +0200

    Update dobasok.py

--- 02_Python/dobasok.py
+++ 02_Python/dobasok.py
@@ -1,6 +1,25 @@
 import random
+MIN = 4
+MAX=8
 
 dobasok = [random.randint(1, 12) for x in range(150)]
 
 print(dobasok)
 
+def kozte(szam, min, max):
+    if szam >= min and szam <= max:
+        return True
+    else:
+        return False
+
+truek = 0
+
+for i in range(150):
+    dobas = random.randint(1, 12)
+    valasz = kozte(dobas, MIN, MAX)
+    if valasz == True:
+        truek += 1
+
+atlag = (truek / 150) *100
+
+print(f'A 150 dobásból {truek} esett 4 és 8 közé. Ez az összes dobás {atlag:.2f}-a.')
